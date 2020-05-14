00e8fb30f1ffadb9dbf92fa3d1d58d1ef3ae65a0
Josh Jones <josh_jones22@live.co.uk>
2020-05-14 23:06:28 +0100

    Add player store

---
+++ src/store/players.ts
@@ -0,0 +1,66 @@
+const initialState = [
+  {
+    name: 'Sam',
+    group: 1,
+    games: 0,
+    points: 0
+  },
+  {
+    name: 'Ross',
+    group: 1,
+    games: 0,
+    points: 0
+  },
+  {
+    name: 'Josh',
+    group: 1,
+    games: 0,
+    points: 0
+  },
+  {
+    name: 'Drewy',
+    group: 1,
+    games: 0,
+    points: 0
+  },
+  {
+    name: 'Bowden',
+    group: 1,
+    games: 0,
+    points: 0
+  },
+  {
+    name: 'Yarwood',
+    group: 2,
+    games: 0,
+    points: 0
+  },
+  {
+    name: 'Rowlands',
+    group: 2,
+    games: 0,
+    points: 0
+  },
+  {
+    name: 'Harri',
+    group: 2,
+    games: 0,
+    points: 0
+  },
+  {
+    name: 'Kyle',
+    group: 2,
+    games: 0,
+    points: 0
+  },
+  {
+    name: 'Ahmed',
+    group: 2,
+    games: 0,
+    points: 0
+  }
+]
+
+export const players = (state=initialState) => {
+  return state
+}
